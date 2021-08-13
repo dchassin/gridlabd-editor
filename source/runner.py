48e20c0360a94a7877dbde9fdf0db3b2d203c526
David P. Chassin <david.chassin@me.com>
2021-08-13 06:17:03 -0700

    Update runner.py

--- source/runner.py
+++ source/runner.py
@@ -3,39 +3,67 @@ import os, sys
 import subprocess as sp
 from concurrent.futures import ThreadPoolExecutor
 
+def do_nothing(*args):
+    pass
+
 class Runner:
 
-    def __init__(self,command,timeout=None,output=sys.stdout,error=sys.stderr):
+    def __init__(self,command,output_call=do_nothing,error_call=do_nothing):
 
-        def log_output(proc):
+        def log_output(self):
 
-            while proc.poll() is None:
-                print("[stdout] ",proc.stdout.read(),flush=True,file=sys.stderr)
+            while self.proc.poll() is None:
+                self.output_call(self.proc.stdout.read())
 
-        def log_error(proc):
+        def log_error(self):
 
-            while proc.poll() is None:
-                print("[strerr] ",proc.stderr.read(),flush=True,file=sys.stderr)
+            while self.proc.poll() is None:
+                self.error_call(self.proc.stderr.read())
 
-        proc = sp.Popen(command.split(), stdout=sp.PIPE, stderr=sp.PIPE, text=True)
-        with ThreadPoolExecutor(2) as pool:
-            print("COMMAND:",command,flush=True,file=sys.stderr)
-            p1 = pool.submit(log_output, proc)
-            p2 = pool.submit(log_error, proc)
+        self.output_call = output_call
+        self.error_call = error_call
+        self.done = False
+        if type(command) is str:
+            self.command = command.split()
+        else:
+            self.command = command
+        self.proc = sp.Popen(self.command, stdout=sp.PIPE, stderr=sp.PIPE, text=True)
+        with ThreadPoolExecutor(2) as self.pool:
+            p1 = self.pool.submit(log_output, self)
+            p2 = self.pool.submit(log_error, self)
             p1.result()
             p2.result()
-        proc.communicate(input=None,timeout=timeout)
+
+    def communicate(self,timeout=None):
+        self.proc.communicate(input=None,timeout=timeout)
+        self.done = True
+
+    def wait(self,timeout=None):
         pool.shutdown(wait=True,cancel_futures=True)
 
 import unittest
 
 class PreferencesTest(unittest.TestCase):
 
+    def output(self,*args):
+        print("OUTPUT:",*args,file=sys.stdout)
+
+    def error(self,*args):
+        print("ERROR:",*args,file=sys.stderr)
+
     def test_1_version(self):
-        runner = Runner("gridlabd --version")
+        command = "gridlabd --version=json"
+        print("COMMAND:",command,flush=True,file=sys.stderr)
+        runner = Runner(command,output_call=self.output,error_call=self.error)
+        runner.communicate()
+        runner.wait()
 
     def test_2_validate(self):
-        runner = Runner("gridlabd -W ../../../slacgismo/gridlabd/module/assert -D keep_progress=TRUE --validate")
+        command = "gridlabd -W ../../../slacgismo/gridlabd/module/assert -D keep_progress=TRUE --validate"
+        print("COMMAND:",command,flush=True,file=sys.stderr)
+        runner = Runner(command,output_call=self.output,error_call=self.error)
+        runner.communicate()
+        runner.wait()
 
 if __name__ == '__main__':
     unittest.main()
