d75a74bcc5c60805647a9850bff86f6aa3a0d49d
David P. Chassin <david.chassin@me.com>
2021-08-23 05:45:15 -0700

    Update pstatus.py

--- source/pstatus.py
+++ source/pstatus.py
@@ -1,6 +1,6 @@
 """Pstatus"""
 
-import os, json
+import os, json, datetime
 import tkinter as tk
 from tkinter import Menu, messagebox, filedialog, simpledialog, ttk, font
 
@@ -12,7 +12,7 @@ import runner
 
 class Pstatus(simpledialog.Dialog):
 
-    def __init__(self,main=None):
+    def __init__(self):
         """Create pstatus viewer
 
         PARAMETERS:
@@ -21,14 +21,10 @@ class Pstatus(simpledialog.Dialog):
 
             Preference object
         """
-        if not main:
-            self.main = tk.Tk()
-            self.main.withdraw()
-        else:
-            self.main = main
+        self.main = None
         self.load()
 
-    def load(self):
+    def load(self,raw=False):
         """Load preferences"
 
         PARAMETERS:
@@ -40,6 +36,29 @@ class Pstatus(simpledialog.Dialog):
             None
         """
         self.pstatus = runner.Runner("gridlabd --pstatus=json",output_format="json").get_output()
+        if not raw:
+            for item, values in self.pstatus.items():
+                if values["status"] in ["RUNNING","PAUSED"]:
+                    try:
+                        start = datetime.datetime.strptime(values["starttime"],"%Y-%m-%d %H:%M:%S %Z")
+                        stop = datetime.datetime.strptime(values["stoptime"],"%Y-%m-%d %H:%M:%S %Z")
+                        now = datetime.datetime.strptime(values["progress"],"%Y-%m-%d %H:%M:%S %Z")
+                        values["progress"] = f"{(now-start).total_seconds()/(stop-start).total_seconds()*100:.0f}%"
+                    except:
+                        values["progress"] = ""
+                    try:
+                        since = datetime.datetime.strptime(values["start"],"%Y-%m-%d %H:%M:%S %Z")
+                        values["runtime"] = f"{datetime.datetime.now()-since}"
+                    except:
+                        values["runtime"] = ""
+                else:
+                    values["progress"] = "-"
+                    values["runtime"] = "-"
+                    values["pid"] = "-"
+                    values["port"] = "-"
+                del values["starttime"]
+                del values["stoptime"]
+                del values["start"]
 
     def __del__(self):
         if self.main:
@@ -47,22 +66,27 @@ class Pstatus(simpledialog.Dialog):
 
     def dialog(self,parent=None):
         if not parent:
-            tk.Tk().withdraw()
+            self.main = tk.Tk()
+            self.main.withdraw()
+        else:
+            self.main = parent
         tk.Toplevel.__init__(self, parent)
-        self.parent = parent
         self.transient(parent)
         self.title("Preferences")
-        self.load()
         headings = list(self.pstatus["0"].keys())
         self.tree = ttk.Treeview(self,columns=headings)
-        self.tree.heading("#0",text="Process")
+        self.tree.heading("#0",text="Job")
         self.tree.column("#0",width=25)
         self.font = font.nametofont("TkDefaultFont")
-        self.width = {"Process":self.font.measure("Process",self) + 10}
-        for hdg in headings:
-            self.width[hdg] = self.font.measure(hdg,self) + 10;
-            self.tree.heading(hdg,text=hdg.title())
         self.refresh(clear=True)
+        self.width = {"#0":50,"pid":50,"progress":50,"status":75,"model":250,"port":50,"runtime":125}
+        for hdg in headings:
+            self.tree.heading(hdg,text=hdg.title())
+            self.tree.column(hdg,width=self.width[hdg],stretch=(hdg=="model"))
+        self.tree.column("#0",width=self.width["#0"],stretch=False)
+        self.tree.pack(side="left",fill="both",expand=True)
+        self.main.after(1000,self.refresh)
+        self.tree.bind("<Button-2>",self.show_popup)
         self.grab_set()
         self.wait_window(self)
 
@@ -71,28 +95,35 @@ class Pstatus(simpledialog.Dialog):
         if clear:
             for item in self.tree.get_children():
                 self.tree.delete(item)
-        headings = list(self.pstatus["0"].keys())
-        for process, values in self.pstatus.items():
+        for job, values in self.pstatus.items():
             values = list(values.values())
-            self.tree.insert('',tk.END,text=process,values=list(map(lambda x:str(x),values)))
-            for hdg,value in zip(headings,values):
-                width = self.font.measure(str(value),self) + 10
-                if self.width[hdg] < width:
-                    self.width[hdg] = width;
-        self.tree.column("#0",width=self.width["Process"])
-        for hdg in headings:
-            self.tree.column(hdg,width=self.width[hdg])
-        self.tree.grid(row=0,column=1)
-        self.main.after(1000,self.refresh)
+            self.tree.insert('',tk.END,text=job,values=list(map(lambda x:str(x),values)))
+
+    def show_popup(self,event):
+        iid = self.tree.identify_row(event.y)
+        pid = self.tree.item(iid,"values")[0]
+        if pid != "-" and int() > 0:
+            popup = Menu(self,tearoff=0);
+            if iid:
+                self.tree.selection_set(iid)
+                popup.add_command(label="Kill",command=self.pkill)
+            try:
+                popup.tk_popup(event.x_root,event.y_root,0)
+            finally:
+                popup.grab_release()
+
+    def pkill(self,idlist=None):
+        if not idlist:
+            idlist = self.tree.selection()
+        for iid in idlist:
+            runner.Runner(f"gridlabd --pkill {iid}")
+
 
 import unittest
 
 class _unittest(unittest.TestCase):
 
-    def test_1_load(self):
-        self.assertEqual(type(Pstatus().pstatus["0"]["port"]),int)
-
-    def test_2_dialog(self):
+    def test_dialog(self):
         Pstatus().dialog()
 
 if __name__ == '__main__':
